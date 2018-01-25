091cd913e909c1858f05bf0f5419c6293399a808
Benedikt Hölker <benedikt.hoelker@gmail.com>
2018-01-25 07:21:27 +0100

    Resolve Redux conflicts

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react'
-import { Button, Container, Grid, Header, Image, Icon, Menu, Message, Sidebar, Segment } from 'semantic-ui-react'
+import React, {Component} from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+  Button,
+  Container,
+  Grid,
+  Header,
+  Image,
+  Icon,
+  Menu,
+  Message,
+  Sidebar,
+  Segment
+} from 'semantic-ui-react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 import MyGrid from './Grid'
 import MyForm from './Form'
 import MyTable from './Table'
 import MySearch from './Search'
 import MySidebar from './Sidebar'
-import firebase, { auth, provider } from './firebase.js';
+import firebase, {auth, provider} from './firebase.js';
+
+import { connect } from 'react-redux'
+import { toggleNavbar } from './actions/uiActions'
 
 class SidebarLeftOverlay extends Component {
   constructor() {
@@ -29,149 +39,180 @@ class SidebarLeftOverlay extends Component {
       workItem: {}
     }
 
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
+    this.login = this
+      .login
+      .bind(this);
+    this.logout = this
+      .logout
+      .bind(this);
   }
 
   componentDidMount() {
-    const samplesRef = firebase.database().ref('samples')
+    const samplesRef = firebase
+      .database()
+      .ref('samples')
 
     samplesRef.on('value', (snapshot) => {
       let samples = snapshot.val()
-      let companies = samples.map(sample => Object.assign({
-        key: sample.company,
-        value: sample.company,
-        text: sample.company
-      }))
+      let companies = samples.map(sample => Object.assign({key: sample.company, value: sample.company, text: sample.company}))
 
-      this.setState({
-        companies: companies,
-        companiesLoading: false
-      });
+      this.setState({companies: companies, companiesLoading: false});
     })
 
-    auth.onAuthStateChanged((user) => {
-      this.setState({user})
-      if (user) {
-        const itemsRef = firebase.database().ref('items/' + this.state.user.uid)
+    auth
+      .getRedirectResult()
+      .then((result) => {
+        let user = result.user
+        this.setState({user})
+        if (user) {
+          const itemsRef = firebase
+            .database()
+            .ref('items/' + this.state.user.uid)
 
-        itemsRef.on('value', (snapshot) => {
-          let items = snapshot.val()
-          let newState = []
-          for (let item in items) {
-            newState.push({
-              id: item,
-              project: items[item].project,
-              subproject: items[item].subproject,
-              scope: items[item].scope,
-              task: items[item].task,
-              description: items[item].description,
-              date: items[item].date,
-              timeStart: items[item].timeStart,
-              timeEnd: items[item].timeEnd,
-              timeSpent: items[item].timeSpent
+          itemsRef.on('value', (snapshot) => {
+            let items = snapshot.val()
+            let newState = []
+            for (let item in items) {
+              newState.push({
+                id: item,
+                project: items[item].project,
+                subproject: items[item].subproject,
+                scope: items[item].scope,
+                task: items[item].task,
+                description: items[item].description,
+                date: items[item].date,
+                timeStart: items[item].timeStart,
+                timeEnd: items[item].timeEnd,
+                timeSpent: items[item].timeSpent
+              });
+            }
+
+            this.setState({
+              items: newState,
+              nextStartTime: newState[newState.length - 1]
+                ? newState[newState.length - 1].timeEnd
+                : new Date().toLocaleTimeString()
             });
-          }
-
-          this.setState({
-            items: newState,
-            nextStartTime: newState[newState.length - 1] ? newState[newState.length - 1].timeEnd : new Date().toLocaleTimeString()
           });
-        });
-      }
-    });
+        }
+      });
   }
 
   logout() {
-    auth.signOut()
+    auth
+      .signOut()
       .then(() => {
-        this.setState({
-          user: null
-        });
+        this.setState({user: null});
       });
   }
 
   login() {
-    auth.signInWithRedirect(provider)
+    auth
+      .signInWithRedirect(provider)
       .then((result) => {
         const user = result.user;
-        this.setState({
-          user
-        });
+        this.setState({user});
       });
   }
 
   handleRemove = (itemId) => {
-    const itemsRef = firebase.database().ref('/items/' + this.state.user.uid + "/" + itemId)
+    const itemsRef = firebase
+      .database()
+      .ref('/items/' + this.state.user.uid + "/" + itemId)
     itemsRef.remove()
   }
 
   handleVMenuItemClick = (id) => {
     console.log(id);
-    this.setState({workItem: {...{id: id}}})
-    firebase.database().ref('/items/'+ this.state.user.uid + "/" + id).once('value', (snapshot) => {
-      this.setState({
-        vMenuActiveItem: id,
-        workItem: { ...snapshot.val(), ...{ id: id } }
-      })
-    });
+    this.setState({
+      workItem: {
+        ...{
+          id: id
+        }
+      }
+    })
+    firebase
+      .database()
+      .ref('/items/' + this.state.user.uid + "/" + id)
+      .once('value', (snapshot) => {
+        this.setState({
+          vMenuActiveItem: id,
+          workItem: {
+            ...snapshot.val(),
+            ...{
+              id: id
+            }
+          }
+        })
+      });
   }
 
-  handleHMenuItemClick = (e, { name }) => this.setState({ hMenuActiveItem: name })
+  handleHMenuItemClick = (e, {name}) => this.setState({hMenuActiveItem: name})
 
-  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+  /* toggleVisibility = () => this.setState({
+    visible: !this.state.visible
+  }) */
 
   render() {
     const state = this.state
     return (
       <Router>
         <Sidebar.Pushable as={Segment}>
-          {this.state.user ?
-          <MySidebar visible={state.visible} items={state.items} handleItemClick={this.handleVMenuItemClick} activeItem={state.vMenuActiveItem} />
-          : ""}
+          {this.state.user
+            ? <MySidebar
+                visible={this.props.visible}
+                items={state.items}
+                handleItemClick={this.handleVMenuItemClick}
+                activeItem={state.vMenuActiveItem}/>
+            : ""}
           <Sidebar.Pusher>
             <Segment basic>
               <Menu stackable>
-                <Menu.Item icon='sidebar' onClick={this.toggleVisibility} />
+                <Menu.Item icon='sidebar' onClick={this.props.toggleVisibility}/>
+                <Menu.Item icon='external' onClick={this.props.toggleVisibility}/>
                 <Menu.Item header as='h3'>Arbeit</Menu.Item>
-                <Menu.Item as={Link} to='/create' name='erfassung' active={this.state.hMenuActiveItem === 'erfassung'} onClick={this.handleHMenuItemClick} />
-                <Menu.Item as={Link} to='/' name='auswertung' active={this.state.hMenuActiveItem === 'auswertung'} onClick={this.handleHMenuItemClick} />
-                {this.state.user
-                  ? <Menu.Item onClick={this.logout} position='right' >{this.state.user.displayName} - Logout</Menu.Item>
+                <Menu.Item
+                  as={Link}
+                  to='/create'
+                  name='erfassung'
+                  active={this.state.hMenuActiveItem === 'erfassung'}
+                  onClick={this.handleHMenuItemClick}/>
+                <Menu.Item
+                  as={Link}
+                  to='/'
+                  name='auswertung'
+                  active={this.state.hMenuActiveItem === 'auswertung'}
+                  onClick={this.handleHMenuItemClick}/> {this.state.user
+                  ? <Menu.Item onClick={this.logout} position='right'>{this.state.user.displayName}
+                      - Logout</Menu.Item>
                   : <Menu.Item onClick={this.login} position='right'>Login</Menu.Item>
-                }
+}
               </Menu>
               {this.state.user
-                  ?
-              <div>
-              <Route exact path="/create" render={(routeProps) => (
-                <MyForm {...routeProps} {
-                  ...{
-                    companies: this.state.companies,
-                    companiesLoading: this.state.companiesLoading,
-                    nextStartTime: this.state.nextStartTime,
-                    user: this.state.user,
-                    workItem: this.state.workItem
-                  }} />
-              )} />
-              <Route exact path="/" render={(routeProps) => (
-                <MyTable {...routeProps} {...{
-                  items: this.state.items,
-                  handleRemove: this.handleRemove,
-                  user: this.state.user
-                }} />
-              )} />
-              </div>
-              
-              : <Message>
-              <Message.Header>
-                Nicht eingeloggt
-              </Message.Header>
-              <p>
-                Sie müssen eingeloggt sein, um die Anwendung zu nutzen
-              </p>
-            </Message>
-              }
+                ? <div>
+                    <Route
+                      exact
+                      path="/create"
+                      render={(routeProps) => (<MyForm
+                      {...routeProps}
+                      { ...{ companies: this.state.companies, companiesLoading: this.state.companiesLoading, nextStartTime: this.state.nextStartTime, user: this.state.user, workItem: this.state.workItem }}/>)}/>
+                    <Route
+                      exact
+                      path="/"
+                      render={(routeProps) => (<MyTable
+                      {...routeProps}
+                      {...{ items: this.state.items, handleRemove: this.handleRemove, user: this.state.user }}/>)}/>
+                  </div>
+
+                : <Message>
+                  <Message.Header>
+                    Nicht eingeloggt
+                  </Message.Header>
+                  <p>
+                    Sie müssen eingeloggt sein, um die Anwendung zu nutzen
+                  </p>
+                </Message>
+}
             </Segment>
           </Sidebar.Pusher>
         </Sidebar.Pushable>
@@ -180,4 +221,24 @@ class SidebarLeftOverlay extends Component {
   }
 }
 
-export default SidebarLeftOverlay
+
+const mapStateToProps = state => {
+  return {
+    visible: state.isNavbarVisible
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleVisibility: () => {
+      dispatch(toggleNavbar())
+    }
+  }
+}
+
+export default SidebarLeftOverlay = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SidebarLeftOverlay)
+
+
