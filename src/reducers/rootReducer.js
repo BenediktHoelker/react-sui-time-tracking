914b4214a01c3543fdf6dd9553a2eae93b071647
Benedikt Hölker <benedikt.hoelker@gmail.com>
2018-02-13 07:30:17 +0100

    Integrate redux-form

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,14 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import categorization from './categorization';
-import records from './records';
-import ui from './ui';
+import { combineReducers } from "redux";
+import { reducer as form } from "redux-form";
+import auth from "./auth";
+import categorization from "./categorization";
+import records from "./records";
+import ui from "./ui";
 
 const rootReducer = combineReducers({
   auth,
   categorization,
+  form,
   records,
   ui
 });
