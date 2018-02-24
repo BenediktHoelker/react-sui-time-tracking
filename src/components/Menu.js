009ad0a1da3b093caca38ac3e370d9589792ef1a
Benedikt Hölker <benedikt.hoelker@gmail.com>
2018-02-24 16:52:00 +0100

    Open Kibana dashboard

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import { Button, Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+
+const openInNewTab = () => {
+  const win = window.open(
+    "http://35.226.8.204/elk/app/kibana#/dashboard/a94f2240-196f-11e8-81c7-13da0e6a4997?embed=true&_g=()",
+    "_blank"
+  );
+  win.focus();
+};
 
 const HorizontalMenu = props => (
   <Menu stackable>
@@ -29,6 +37,9 @@ const HorizontalMenu = props => (
       active={props.activeItem === "tage"}
       onClick={props.handleItemClick}
     />
+
+    <Menu.Item name="kibana" onClick={openInNewTab} text="Kibana" />
+
     {props.user ? (
       <Menu.Item onClick={props.logout} position="right">
         {props.user.displayName}
